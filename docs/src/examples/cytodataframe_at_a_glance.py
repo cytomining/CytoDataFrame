13894b50bc4eac44ba52196e972bbd098919848b
Dave Bunten <dave.bunten@cuanschutz.edu>
2025-11-06 15:00:58 -0700

    Update scale bar in example notebook (#144)
    * update scale bar in example notebook
    
    * rerun with pull from main

--- docs/src/examples/cytodataframe_at_a_glance.py
+++ docs/src/examples/cytodataframe_at_a_glance.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.17.3
+#       jupytext_version: 1.18.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -52,6 +52,7 @@ frame = CytoDataFrame(
 ][:3]
 frame
 
+# %%time
 # view JUMP plate BR00117006 with images and overlaid outlines for segmentation
 CytoDataFrame(
     data=f"{jump_data_path}/BR00117006_shrunken.parquet",
@@ -94,9 +95,9 @@ CytoDataFrame(
     data_context_dir=f"{jump_data_path}/images/orig",
     data_outline_context_dir=f"{jump_data_path}/images/outlines",
     display_options={
-        "pixel_per_um": 0.1550,
+        "um_per_pixel": 0.1550,
         "scale_bar": {
-            "length_um": 100,
+            "length_um": 5,
             "location": "lower right",
             "color": (255, 255, 255),
             "thickness_px": 2,
